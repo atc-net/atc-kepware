7ee0e5abfaec5b9517868359f5ae2fa47501ef59
Per Kops <pko@delegate.dk>
2022-03-22 15:05:16 +0100

    fix: Removing Default properties from ChannelBase and fixing a documentation summary

--- src/Atc.Kepware.Configuration/Contracts/Drivers/ChannelBase.cs
+++ src/Atc.Kepware.Configuration/Contracts/Drivers/ChannelBase.cs
@@ -24,7 +24,7 @@ public class ChannelBase
     public bool? CaptureDiagnostics { get; set; }
 
     /// <summary>
-    /// The total number of user defined tags assigned to the device.
+    /// The total number of user defined tags assigned to the channel and all of its devices.
     /// </summary>
     [JsonPropertyName("servermain.CHANNEL_STATIC_TAG_COUNT")]
     public int TagCount { get; set; }
@@ -33,19 +33,19 @@ public class ChannelBase
     /// Choose how write data is passed to the underlying communications driver when more than one write exists in the write queue.
     /// </summary>
     [JsonPropertyName("servermain.CHANNEL_WRITE_OPTIMIZATIONS_METHOD")]
-    public ChannelOptimizationMethodType OptimizationMethod { get; set; } = ChannelOptimizationMethodType.WriteOnlyLatestValueForAllTags;
+    public ChannelOptimizationMethodType OptimizationMethod { get; set; }
 
     /// <summary>
     /// Specify the ratio of write operations to read operations, based on one read per configurable number of writes.
     /// </summary>
     [JsonPropertyName("servermain.CHANNEL_WRITE_OPTIMIZATIONS_DUTY_CYCLE")]
-    public int WriteOptimizationDutyCycle { get; set; } = 10;
+    public int WriteOptimizationDutyCycle { get; set; }
 
     /// <summary>
     /// Choose how to send invalid floating-point numbers to the client.
     /// </summary>
     [JsonPropertyName("servermain.CHANNEL_NON_NORMALIZED_FLOATING_POINT_HANDLING")]
-    public ChannelNonNormalizedFloatingPointHandlingType NonNormalizedFloatingPointHandling { get; set; } = ChannelNonNormalizedFloatingPointHandlingType.ReplaceWithZero;
+    public ChannelNonNormalizedFloatingPointHandlingType NonNormalizedFloatingPointHandling { get; set; }
 
     /// <inheritdoc />
     public override string ToString()
