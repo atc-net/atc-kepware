4be64da5fbb930984782bbf7e497416d4fb6dc79
David Kallesen <david@lmdk.dk>
2022-03-24 00:34:05 +0100

    feat: Improve error message - when content is not json as first expected

--- src/Atc.Kepware.Configuration/GlobalUsings.cs
+++ src/Atc.Kepware.Configuration/GlobalUsings.cs
@@ -3,6 +3,7 @@ global using System.ComponentModel.DataAnnotations;
 global using System.Diagnostics.CodeAnalysis;
 global using System.Net;
 global using System.Net.Http.Headers;
+global using System.Net.Mime;
 global using System.Text;
 global using System.Text.Json;
 global using System.Text.Json.Serialization;
