960560baf5f456cd21de890146f0305a99057d76
Per Kops <pko@delegate.dk>
2022-10-18 12:04:34 +0200

    fix: Added missing documentation headers to IotAgentRestClient and marking as Sealed

--- src/Atc.Kepware.Configuration.Contracts/IotGateway/IotAgentRestClient.cs
+++ src/Atc.Kepware.Configuration.Contracts/IotGateway/IotAgentRestClient.cs
@@ -1,25 +1,35 @@
 namespace Atc.Kepware.Configuration.Contracts.IotGateway;
 
-public class IotAgentRestClient : IotAgentBase, IIotAgentRestClient
+public sealed class IotAgentRestClient : IotAgentBase, IIotAgentRestClient
 {
+    /// <inheritdoc />
     public string Url { get; set; } = string.Empty;
 
+    /// <inheritdoc />
     public IotAgentPublishHttpMethodType PublishHttpMethod { get; set; }
 
+    /// <inheritdoc />
     public int Rate { get; set; }
 
+    /// <inheritdoc />
     public IotAgentPublishFormatType PublishFormat { get; set; }
 
+    /// <inheritdoc />
     public int MaxEventsPerPublish { get; set; }
 
+    /// <inheritdoc />
     public int TransactionTimeout { get; set; }
 
+    /// <inheritdoc />
     public bool SendInitialUpdate { get; set; }
 
+    /// <inheritdoc />
     public string HttpHeaders { get; set; } = string.Empty;
 
+    /// <inheritdoc />
     public IotAgentPublishMessageFormatType PublishMessageFormat { get; set; }
 
+    /// <inheritdoc />
     public IotAgentPublishMediaType PublishMediaType { get; set; }
 
     public override string ToString()
