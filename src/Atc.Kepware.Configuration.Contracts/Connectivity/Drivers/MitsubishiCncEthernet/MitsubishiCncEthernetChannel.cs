ec08f60f68fff0ed4b2c3d430bd753920861cfa0
Per Kops <per_kops@hotmail.com>
2026-01-27 00:30:29 -0800

    feat(contracts): add Mitsubishi CNC Ethernet driver support

---
+++ src/Atc.Kepware.Configuration.Contracts/Connectivity/Drivers/MitsubishiCncEthernet/MitsubishiCncEthernetChannel.cs
@@ -0,0 +1,8 @@
+namespace Atc.Kepware.Configuration.Contracts.Connectivity.Drivers.MitsubishiCncEthernet;
+
+/// <summary>
+/// Mitsubishi CNC Ethernet channel.
+/// </summary>
+public class MitsubishiCncEthernetChannel : ChannelBase, IMitsubishiCncEthernetChannel
+{
+}
---
+++ src/Atc.Kepware.Configuration.Contracts/Interfaces/Connectivity/MitsubishiCncEthernet/IMitsubishiCncEthernetDevice.cs
@@ -0,0 +1,8 @@
+namespace Atc.Kepware.Configuration.Contracts.Interfaces.Connectivity.MitsubishiCncEthernet;
+
+/// <summary>
+/// Defines the Mitsubishi CNC Ethernet device properties.
+/// </summary>
+public interface IMitsubishiCncEthernetDevice : IDeviceBase, IMitsubishiCncEthernetDeviceRequest
+{
+}
