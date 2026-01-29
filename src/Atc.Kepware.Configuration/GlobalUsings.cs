348fb23dda8e839bdb12c133caade3485346f52c
Per Kops <pko@delegate.dk>
2026-01-29 19:10:48 +0100

    fix: add missing global usings

--- src/Atc.Kepware.Configuration/GlobalUsings.cs
+++ src/Atc.Kepware.Configuration/GlobalUsings.cs
@@ -212,3 +212,4 @@ global using Mapster;
 global using Microsoft.Extensions.DependencyInjection;
 global using Microsoft.Extensions.Logging;
 global using Microsoft.Extensions.Logging.Abstractions;
+global using Microsoft.Extensions.Options;
